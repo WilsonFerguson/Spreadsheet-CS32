79db65b5864415fe2220845264496a0cf64c689d
BuildTools <unconfigured@null.spigotmc.org>
2023-03-23 15:31:04 -0700

    test

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -6,7 +6,7 @@ public class Spreadsheet {
 	public static Scanner console = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		Sheet sheet = new Sheet(); // Keep this as the first statement in main
+		Sheet sheet = new Sheet();
 		// TODO in checkpoint 1: finish implementing by adding your own code here
 		// with a loop to read lines of user input until the user enters quit,
 		// calling sheet.processCommand with each line and printing the result to the
